0ce7ae27ea221a4ea12d83eac1f82210479fde35
Andrei-Gheorghe Brunchea <508450@student.fontys.nl>
2025-03-04 09:00:34 +0200

    test 1

--- src/Form.java
+++ src/Form.java
@@ -7,39 +7,39 @@ public class Form {
         this.color = "white";
     }
 
-    // Constructor cu parametri
+    
     public Form(String color) {
         this.color = color;
     }
 
-    // Metodă care returnează aria (implicit 0)
+
     public float getArea() {
         return 0;
     }
 
-    // Suprascrierea metodei toString()
+
     @Override
     public String toString() {
         return "This form has the color " + color;
     }
 
-    // Getter pentru color (opțional)
+
     public String getColor() {
         return color;
     }
 
-    // Setter pentru color (opțional)
+
     public void setColor(String color) {
         this.color = color;
     }
 
-    // Testare în main
-    public static void main(String[] args) {
-        Form f1 = new Form();  // Obiect creat cu constructorul implicit
-        Form f2 = new Form("red");  // Obiect creat cu constructorul cu parametru
 
-        System.out.println(f1);  // Afișează: This form has the color white
-        System.out.println(f2);  // Afișează: This form has the color red
+    public static void main(String[] args) {
+        Form f1 = new Form();
+        Form f2 = new Form("red");
+
+        System.out.println(f1);
+        System.out.println(f2);
         System.out.println("Aria formei: " + f1.getArea());
     }
 }
